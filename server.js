e44f077fba9fb2c5912d0efc2554abd767c5fd3f
Mohammad-Almomani <moh.moma.mmm@gmail.com>
2022-08-09 16:51:44 +0300

    created express server and connected it to mongo

--- server.js
+++ server.js
@@ -3,16 +3,28 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
+
+const createBook = require('./Moduels/Schema');
 
 const app = express();
 app.use(cors());
 
+createBook();
+
 const PORT = process.env.PORT || 3001;
+app.get('/books', (req,res)=>{
+  const bookModel = mongoose.model('bookModel');
 
-app.get('/test', (request, response) => {
-
-  response.send('test request received')
+  bookModel.find({}, (error, data) => {
+    if (error) console.log(`error reading from the db: ${error}`);
+    else res.send(data);
+})
 
 })
+
+
+
+
 
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
